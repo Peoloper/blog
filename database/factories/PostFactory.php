0c970cb32d9aac31338022051d46dcecc77d804e
Peoloper <Peoloperteen@example.com>
2022-01-07 14:07:04 +0100

    new factories

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+
+    protected $model = Post::class;
+
+    public function definition()
+    {
+        $title = $this->faker->text(10);
+        $slug = Str::slug($title);
+        return [
+            'title' => $title,
+            'slug' => $slug,
+            'content' => $this->faker->text(300),
+            'created_at'  => Carbon::now(),
+            'updated_at'  => Carbon::now(),
+
+            'user_id' => function() {
+                return User::factory()->create()->id;
+            },
+            'category_id' => function(){
+                return Category::factory()->create()->id;
+            }
+        ];
+    }
+}
