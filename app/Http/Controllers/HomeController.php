74e3b478522abfb2a562fd896c6df45b4eeb9dc7
Peoloper <Peoloperteen@example.com>
2022-01-10 11:22:40 +0100

    admin lte

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,15 @@ class HomeController extends Controller
      */
     public function index()
     {
+       // $role = Role::create(['name' => 'writer']);
+       // $permisson = Permission::create(['name' => 'edit post']);
+       // $role = Role::findById(1);
+        //$permisson = Permission::findById(1);
+       // $role->givePermissionTo($permisson);
+
+       // auth()->user()->givePermissionTo('edit post');
+        //auth()->user()->assignRole('writer');
+       // dump(auth()->user()->permissions());
         return view('home');
     }
 }
