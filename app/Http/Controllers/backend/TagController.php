2634f84e4827c3098da316aa710c6d073545d6d0
Peoloper <Peoloperteen@example.com>
2022-01-10 11:31:17 +0100

    new controllers

--- app/Http/Controllers/backend/TagController.php
+++ app/Http/Controllers/backend/TagController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class DashboardController extends Controller
+class TagController extends Controller
 {
     /**
      * Display a listing of the resource.
