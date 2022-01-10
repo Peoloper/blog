15fc42806fd97d44714f44664fb1f6aad154ab43
Peoloper <Peoloperteen@example.com>
2022-01-10 11:32:14 +0100

    updated PostController

--- app/Http/Controllers/backend/PostController.php
+++ app/Http/Controllers/backend/PostController.php
@@ -2,14 +2,97 @@
 
 namespace App\Http\Controllers\backend;
 
+use App\DataTables\backend\PostsDataTable;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(PostsDataTable $dataTable)
     {
-        return view('backend.post.index',['posts' => Post::paginate(15)]);
+        return $dataTable->render('backend.post.index');
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('backend.post.create', [
+            'categories' => Category::all(),
+            'tags' => Tag::all()
+            ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Post $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('backend.post.show', ['post' => $post]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  Post $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        toast('Your Post as been submited!','success');
+        return view('backend.post.edit', [
+            'post' => $post,
+            'categories' => Category::all(),
+            'tags' => Tag::all()
+            ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Post $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Post $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
     }
 }
