54910311788f10a40617a5fc4528d1735542bfe3
Peoloper <Peoloperteen@example.com>
2022-01-13 00:55:45 +0100

    created PostRequest

---
+++ app/Http/Requests/PostRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => [
+                'required',
+                'string',
+                'max: 50'
+            ],
+            'content' => [
+                'required',
+                'string',
+                'max: 1000'
+            ],
+            'category_id' => [
+                'required',
+                'integer'
+            ],
+            'tags' => [
+                'required',
+                'max:8'
+            ]
+            ,
+            'image' => request()->isMethod('put') ? 'nullable|mimes:jpeg,jpg,png,gif,svg|max:8000' : 'required|mimes:jpeg,jpg,png,gif,svg|max:8000'
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+}
