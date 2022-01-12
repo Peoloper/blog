9dbf58cc7d87726cd0f8972bcba3c5133a50f04d
Peoloper <Peoloperteen@example.com>
2022-01-13 00:59:48 +0100

    updated index post

--- resources/views/backend/post/index.blade.php
+++ resources/views/backend/post/index.blade.php
@@ -1,12 +1,42 @@
 @extends('layouts.backend')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12 col-md-12">
-                {!! $dataTable->table() !!}
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="">Home</a></li>
+                        <li class="breadcrumb-item active">Posts list</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h3 class="card-title">Posts List</h3>
+                                @can('write post')
+                                    <a href="{{route('admin.post.create')}}" class="btn btn-primary">Create Post</a>
+                                @endcan
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="col-md-12">
+                                {!! $dataTable->table() !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
     {!! $dataTable->scripts() !!}
 @endsection
+
