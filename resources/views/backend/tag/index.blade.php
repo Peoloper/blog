efc173688e53473eb8ff30b7eb6cded759460b56
Peoloper <Peoloperteen@example.com>
2022-01-13 00:53:10 +0100

    created files tag blade

---
+++ resources/views/backend/tag/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.backend')
+
+@section('content')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="">Home</a></li>
+                        <li class="breadcrumb-item active">Tags list</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h3 class="card-title">Tags List</h3>
+                                <a href="{{route('admin.tag.create')}}" class="btn btn-primary">Create Tag</a>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="col-md-12">
+                                {!! $dataTable->table() !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {!! $dataTable->scripts() !!}
+@endsection
