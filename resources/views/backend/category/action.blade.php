cf3f58703442b9f400d3c34c842a22dd3b94b41d
Peoloper <Peoloperteen@example.com>
2022-01-13 00:51:20 +0100

    created files blade category

---
+++ resources/views/backend/category/action.blade.php
@@ -0,0 +1,4 @@
+<div class="d-flex order-actions">
+    <a href="{{route('admin.category.edit', $model)}}" class="btn btn-sm btn-primary mr-1"><i class="fas fa-edit"></i></a>
+    <a href="{{route('admin.category.destroy', $model)}}" class="btn btn-sm btn-danger mr-1" id="delete" data-table="category-table"><i class="fas fa-trash"></i></a>
+</div>
---
+++ resources/views/backend/category/edit.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.backend')
+
+@section('content')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="">Home</a></li>
+                        <li class="breadcrumb-item"><a href="">Categories list</a></li>
+                        <li class="breadcrumb-item active">Edit Category</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h3 class="card-title">Edit Category - {{ $category->name }} </h3>
+                                <a href="{{route('admin.post.index')}}" class="btn btn-primary">Go Back to Post List</a>
+                            </div>
+                        </div>
+                        <div class="card-body p-0">
+                            <div class="row">
+                                <div class="col-12 col-lg-6 offset-lg-3 col-md-8 offset-md-2">
+                                    @if ($errors->any())
+                                        <div class="alert alert-danger">
+                                            <ul>
+                                                @foreach ($errors->all() as $error)
+                                                    <li>{{ $error }}</li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    @endif
+                                    <div class="card-body">
+                                        <form action="{{route('admin.category.update',['category' => $category->id])}}" method="POST" enctype="multipart/form-data">
+                                            @csrf
+                                            @method('PUT')
+                                            <div class="form-group">
+                                                <label for="name">Category name</label>
+                                                <input type="name" name="name" class="form-control" value="{{ $category->name }}" placeholder="Enter name">
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="row">
+                                                    <div class="col-8">
+                                                        <label for="image">Image</label>
+                                                        <div class="custom-file">
+                                                            <input type="file" name="image" class="custom-file-input" id="image">
+                                                            <label class="custom-file-label" for="image">Choose file</label>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-4 text-right">
+                                                        <div style="max-width: 200px; max-height: 100px;overflow:hidden; margin-left: auto">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="card-footer">
+                                                <button type="submit" class="btn btn-lg btn-primary">Update Category</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
