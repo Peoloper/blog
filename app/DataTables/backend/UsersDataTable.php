eeb9ce485a82266cb6b46488043760201259daf3
Peoloper <Peoloperteen@example.com>
2022-01-18 10:23:11 +0100

    added UsersDataTable

---
+++ app/DataTables/backend/UsersDataTable.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\DataTables\backend;
+
+use App\Models\User;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class UsersDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables()
+            ->eloquent($query)
+            ->addColumn('action', function (User $model)
+            {
+                return view('backend.user.action', compact('model'));
+            })
+            ->editColumn('created_at', function($model){
+                return $model->created_at->diffForHumans();
+            })
+            ->editColumn('Role', function ($model)
+            {
+                return $model->getRoleNames();
+            })
+            ->filterColumn('role_id', function ($query, $keyword)
+            {
+                $query->whereHas('role', function ($q) use ($keyword)
+                {
+                    $q->where('name', 'like', "%{$keyword}%");
+                });
+            });
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\Models\User $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(User $model)
+    {
+        return $model::with(['roles'])->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+            ->setTableId('users-table')
+            ->columns($this->getColumns())
+            ->buttons(["csv", "excel", "pdf", "print"])
+            ->minifiedAjax()
+            ->dom('Bfrtlip')
+            ->orderBy(1, 'asc');
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            Column::make('id'),
+            Column::make('name')->title('Name'),
+            Column::make('email')->title('Email'),
+            Column::make('Role', 'role_id'),
+            Column::make('created_at')->title('Added'),
+            Column::computed('action')
+                ->exportable(false)
+                ->printable(false)
+                ->addClass('text-center')->title('Action'),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'backend\Users_' . date('YmdHis');
+    }
+}
