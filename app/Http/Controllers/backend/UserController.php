b451f312cee8c9696886058c9b4cee85701a9918
Peoloper <Peoloperteen@example.com>
2022-01-18 10:23:34 +0100

    added UserController

---
+++ app/Http/Controllers/backend/UserController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\DataTables\backend\UsersDataTable;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(UsersDataTable $dataTable)
+    {
+        return $dataTable->render('backend.user.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $roles = Role::all();
+
+        return view('backend.user.create', ['roles' => $roles]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  UserRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UserRequest $request)
+    {
+        $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+
+        $user->assignRole($data['role']);
+
+        return redirect()->route('admin.user.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        $roles = Role::all();
+        return view('backend.user.show', ['user' => $user, 'roles' => $roles ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        $roles = Role::all();
+
+        return view('backend.user.edit', ['user' => $user,'roles' => $roles]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  UserRequest $request
+     * @param  User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password'])
+        ]);
+
+       $user->syncRoles($request['role']);
+
+        return redirect()->route('admin.user.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json('elo');
+    }
+}
