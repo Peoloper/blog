46efd9fc96f5a02d84aa3ea7b9fc6bbba6e721dc
Peoloper <Peoloperteen@example.com>
2022-01-20 18:57:36 +0100

    updated Rolerequest

--- app/Http/Requests/RoleRequest.php
+++ app/Http/Requests/RoleRequest.php
@@ -14,9 +14,12 @@ class RoleRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => request()->isMethod('put') ? 'nullable' : 'required|unique:roles|max:20',
+            'name' => request()->isMethod('put')
+                ? 'required|string|min:5|max:20|unique:roles,name,'.$this->role->id
+                : 'required|string|unique:roles|min:5|max:20',
+
             'permissions' => [
-                'required'
+                'required',
             ]
         ];
     }
